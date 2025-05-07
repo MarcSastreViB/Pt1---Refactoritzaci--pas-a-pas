661559ea030fcf43d0e71cf5e7e47d71429460b5
Marc <masa863@vidalibarraquer.net>
2025-05-07 12:37:18 +0200

    Constantes y variables temporales bien usadas

--- Refactorizar.cs
+++ Refactorizar.cs
@@ -19,12 +19,12 @@ class Refactorizar
 	}
 	static void MostrarInformacion(string nombre, int edad, double salario)
 	{
+		const double TASA_AUMENTO = 0.05;
 		Console.WriteLine("Bienvenido: " + nombre);
 		Console.WriteLine("Edad: " + edad);
 		Console.WriteLine("Salario: " + salario);
-		double aumento = 0.05;
-		salario = salario + salario * aumento;
-		Console.WriteLine("Nuevo salario: " + salario);
+		double nuevoSalario = salario + salario * TASA_AUMENTO;
+		Console.WriteLine("Nuevo salario: " + nuevoSalario);
 	}
 
 	static bool esMayor(string n, int e)
