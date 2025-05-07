55d56e902a979de82933352f2da99d72b9c8e27c
Marc <masa863@vidalibarraquer.net>
2025-05-07 12:25:31 +0200

    Inici

---
+++ Refactorizar.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Refactorizar
+{
+	public static void Main()
+	{
+    	string nombre = "Juan";
+    	int edad = 45;
+    	double salario = 2500;
+
+    	if (esMayor(nombre, edad)) {
+        	Console.WriteLine("Bienvenido: " + nombre);
+        	Console.WriteLine("Edad: " + edad);
+        	Console.WriteLine("Salario: " + salario);
+        	double aumento = 0.05;
+        	salario = salario + salario * aumento;
+        	Console.WriteLine("Nuevo salario: " + salario);
+    	}
+	}
+
+	static bool esMayor(string n, int e)
+	{
+    	return e >= 18;
+	}
+}
